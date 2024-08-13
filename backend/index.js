3dc01e35b3f680209aa073ecbdd5c17778cdab5b
karthikwadeyar2 <karthik.wadeyar2@cognizant.com>
2024-08-13 11:36:36 +0530

    Added Register login logout routes and its functionality

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,7 @@ import cors from 'cors'
 import cookieParser from 'cookie-parser'
 import dotenv from 'dotenv'
 import connectDB from './utils/db.js'
+import userRoute from './routes/user.route.js'
 
 dotenv.config({})
 
@@ -27,7 +28,7 @@ const corsOptions = {
 }
 app.use(cors(corsOptions))
 
-
+app.use('/api/v1/user', userRoute)
 
 app.listen(PORT, () => {
     connectDB();
